c30b6289be38bfdd4432a17b864a8c6a77045f58
Meiyanathan <meiyanathan@phoenixstudioz.com>
2021-01-13 11:06:15 +0530

    header component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path='/' component={HomePage}  />
-        <Route  path='/shop' component={ShopPage}  />
-      </Switch>
-      </div>
+      <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage}  />
+          <Route  path='/shop' component={ShopPage}  />
+        </Switch>
+    </div>
   );
 }
 
