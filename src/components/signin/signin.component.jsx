0c922caa4b3d9de0461f6b00ed60d8bda8ff9b88
meiyanathan <meiyanathan.j@apaengineering.com>
2021-02-17 17:40:59 +0530

    redux and implemented userReducer ,userAction to header and app component

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './signin.styles.scss';
 
@@ -15,9 +15,20 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email : '',password: ''});
+        
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email : '',password: ''});
+        }catch (error){
+            console.log(error);
+            alert(error.message);
+        }
+
+        
     }
 
     handleChange = event => {
@@ -52,7 +63,7 @@ class SignIn extends React.Component {
                     />
                     <div className="buttons"> 
                         <CustomButton type='submit' >SIGN IN</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
                             Sign in with Google
                         </CustomButton>
                     </div>
